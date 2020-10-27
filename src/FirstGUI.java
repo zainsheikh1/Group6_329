55fa37a4fa11436006b47fbbb1e4e10b970b68ac
John Hutchinson <john.hutchinson@ucalgary.ca>
2020-10-27 13:38:43 -0600

    Initial Basic GUI
    Made proof of concept GUI using JavaFX

---
+++ src/FirstGUI.java
@@ -0,0 +1,32 @@
+import javafx.application.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.*;
+public class FirstGUI extends Application{
+	public static void main(String[] args) {
+		launch();
+	}
+	public void start(Stage primaryStage) {
+		StackPane root = new StackPane();
+		GridPane grid = new GridPane();
+		Button[] buttons = new Button[25];
+		int currentIndex = 0;
+		for(int i =1;i<6;i++) {
+			for(int j =1; j<6;j++) {
+				Button currentButton = new Button(i+"-"+j);
+				buttons[currentIndex] = currentButton;	
+				grid.add(currentButton, i, j);
+				currentIndex+=1;
+			}
+		}
+		
+		root.getChildren().add(grid);
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
+	}
+	
+	
+}
