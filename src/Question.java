fa9d716dbe2f3e833ec873291b0690b4b4f976e9
zainsheikh1 <47370358+zainsheikh1@users.noreply.github.com>
2020-11-10 16:44:40 -0700

    Add files via upload

--- src/Question.java
+++ src/Question.java
@@ -1,13 +1,27 @@
-
-
+import java.util.ArrayList;
 
 public class Question {
-
-public String question;
-public String answer;
-public String[] possibleAnswers;
-public int value;
-
-
+	private String type;
+	private String category;
+	private String question;
+	private String answer;
+	private ArrayList<String> answerList = new ArrayList<String>();
+	private String explanation;
+	
+	public Question(String category, String type, String question, String answer, ArrayList<String> answerList, String explanation) {
+		this.category = category;
+		this.type = type;
+		this.question = question;
+		this.answer = answer;
+		this.answerList = answerList;
+		this.explanation = explanation;
+	}
+	
+	public String getCategory() {return this.category;}
+	public String getType() {return this.type;}
+	public String getQuestion() {return this.question;}
+	public String getAnswer() {return this.answer;}
+	public ArrayList<String> getAnswerList() {return this.answerList;}
+	public String getExplanation() {return this.explanation;}
 
 }
